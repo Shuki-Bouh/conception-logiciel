cbabe14a1bf0b256e741a915fcdf799719265086
Matt <matt.musy@gmail.com>
2024-02-26 14:05:23 +0100

    excel reader

--- src/main/Serveur/Bioreacteur.java
+++ src/main/Serveur/Bioreacteur.java
@@ -1,17 +1,120 @@
 package src.main.Serveur;
 
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
 public class Bioreacteur {
 
-    public void Bioreacteur() {
+    private ArrayList<Float> floatList = new ArrayList<>();
+    private ArrayList<String> stringList = new ArrayList<>();
+    public Bioreacteur() {
+        lire("src/main/ressources/2022-10-03-Act2-1.xlsx",7,20,3);
+        System.out.println("Autre opération");
+        lire_str_ligne("src/main/ressources/2022-10-03-Act2-1.xlsx",20,0);
 
     }
 
-    private void lire(String file) {
+    private ArrayList lire(String path,int startRow ,int endRow,int Column) {
+        try (FileInputStream file = new FileInputStream(path)) {
+            Workbook workbook = WorkbookFactory.create(file);
+            Sheet sheet = workbook.getSheetAt(0);
 
+            // Check if param1 is provided, if not, set a default value of 10
+            if (startRow <= 6) {
+                startRow = 7;
+            }
+
+            // Check if param2 is provided, if not, set a default value of "default"
+            if (endRow == 0 ) {
+                endRow = 5819;
+            }
+
+            if (endRow>5819){
+                endRow=5819;
+            }
+
+            int startColumn = Column;
+            int endColumn = Column;
+
+            for (int i = startRow; i <= endRow; i++) {
+                Row row = sheet.getRow(i);
+                if (row != null) {
+                    for (int j = startColumn; j <= endColumn; j++) {
+                        Cell cell = row.getCell(j);
+                        if (cell != null) {
+                            System.out.print(cell.toString() + "\t");
+                            try {
+                                float floatValue = Float.parseFloat(cell.toString());
+                                floatList.add(floatValue);
+                            } catch (NumberFormatException e) {
+                                System.out.println("Une valeure dans le tableau n'est pas un float");
+                            }
+                        }
+                        System.out.println();
+                    }
+                }
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return floatList;
     }
+    private ArrayList lire_str_ligne(String path,int Row,int Column ) {
+        // Ligne 20 en excel est de 19 en entrée et la colonne 1 sur excel est de 0 ici
+        try (FileInputStream file = new FileInputStream(path)) {
+            Workbook workbook = WorkbookFactory.create(file);
+            Sheet sheet = workbook.getSheetAt(0);
 
+            // Check if param1 is provided, if not, set a default value of 10
+            int startRow;
+            if (Row <= 6) {
+                startRow = 7;
+            }
+            else {
+                startRow=Row;
+            }
+
+            int endRow=startRow;
+            int startColumn = Column;
+            int endColumn = Column+3;
+
+            for (int i = startRow; i <= endRow; i++) {
+                Row row = sheet.getRow(i);
+                if (row != null) {
+                    for (int j = startColumn; j <= endColumn; j++) {
+                        Cell cell = row.getCell(j);
+                        if (cell != null) {
+                            System.out.print(cell.toString() + "\t");
+                            try {
+                                stringList.add(cell.toString());
+                            } catch (IllegalArgumentException e) {
+                                System.out.println("Une valeure dans le tableau n'est pas un String");
+                            }
+                        }
+                        System.out.println();
+                    }
+                }
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+        return stringList;
+    }
     private void organiser() {
 
     }
 
 }
+
+
+
+
--- src/main/Serveur/MainServeur.java
+++ src/main/Serveur/MainServeur.java
@@ -2,9 +2,12 @@ package src.main.Serveur;
 
 public class MainServeur {
 
+	private static Bioreacteur bioreact;
 	public static void main(String[] args) {
+		bioreact=new Bioreacteur();
 		ServeurTCP myServ = new ServeurTCP(new UnContexte() , new ProtocolePingPong() , 6666 );
 		myServ.start();
+
 		
 	}
 }
